b34d2c3b5cd7957de6b3f03b337366eae0ff90d6
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2025-01-27 19:20:59 +0000

    Stop tracking lazy-lock.json.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,13 +18,14 @@ return {
     lazy = false,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "python",
+        "c", "cpp",
+  		},
+  	},
+  },
 }
