48e00066c0cdcb12b389192e08dc6571efb01a5f
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2025-01-28 12:22:36 +0000

    Add spell checking in British English. Add markdown-preview plugin.

--- lua/options.lua
+++ lua/options.lua
@@ -8,3 +8,7 @@ o.cursorlineopt = 'both' -- to enable cursorline!
 -- Set the foldmethod to expr
 o.foldmethod = 'expr' -- foldmethod=expr
 o.foldexpr = 'nvim_treesitter#foldexpr()' -- foldexpr=nvim_treesitter#foldexpr()
+
+-- Set up spell checking to British English
+o.spell = true
+o.spelllang = 'en_gb'
