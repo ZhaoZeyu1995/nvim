d5524547a5eaea68d712c2a9952a7bf358f6e707
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2025-02-01 21:04:15 +0000

    Set line wrap as false and tab intendation.

--- lua/options.lua
+++ lua/options.lua
@@ -3,12 +3,20 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt = 'both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 
 -- Set the foldmethod to expr
-o.foldmethod = 'expr' -- foldmethod=expr
-o.foldexpr = 'nvim_treesitter#foldexpr()' -- foldexpr=nvim_treesitter#foldexpr()
+o.foldmethod = "expr" -- foldmethod=expr
+o.foldexpr = "nvim_treesitter#foldexpr()" -- foldexpr=nvim_treesitter#foldexpr()
 
 -- Set up spell checking to British English
 o.spell = true
-o.spelllang = 'en_gb'
+o.spelllang = "en_gb"
+
+-- Set no line wrap
+o.wrap = false
+
+-- Indentation
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
