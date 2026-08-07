f438d2be81591207be04878afa48c0aa0fa6f4e8
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2026-08-07 22:58:33 +0100

    Revert "Add git blame keymaps for current line and full file"
    This reverts commit fb47c3de0c50fbe57db1eb5797786ce3d423e1f6.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,11 +7,4 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- git blame (gitsigns): press again to jump inside the float
-map("n", "<leader>gb", function()
-  require("gitsigns").blame_line { full = true }
-end, { desc = "git blame current line" })
-
-map("n", "<leader>gB", "<cmd>Gitsigns blame<cr>", { desc = "git blame full file" })
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
